f8414f532fd5dfb6bba42c6bb38cf6389d44afdd
Vlalden <vladlen@vozhzhaev.ru>
2024-08-25 13:37:04 +0300

    add models and admin panel

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, verbose_name="name")
+    description = models.TextField(verbose_name="description")
+    def __str__(self):
+        return self.name
+
+class Brand(models.Model):
+    name = models.CharField(max_length=255, verbose_name="name")
+    description = models.TextField(verbose_name="description")
+    def __str__(self):
+        return self.name
