d06acd8336c2a94acc6b582752e94f8ed87e0ac3
Vlalden <vladlen@vozhzhaev.ru>
2024-08-29 13:34:46 +0300

    add template

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -13,3 +13,8 @@ class Brand(models.Model):
     description = models.TextField(verbose_name="description")
     def __str__(self):
         return self.name
+
+class Article(models.Model):
+    title = models.CharField(max_length=255, verbose_name="title")
+    content = models.TextField(verbose_name="content")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="created_at")
